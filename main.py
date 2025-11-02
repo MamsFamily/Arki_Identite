303f4bba7e3b490c974b5ee51f8886bbef470439
MamsFamily <johannapiednoel1991@gmail.com>
2025-11-02 15:26:17 +0000

    Ajout de la base persistante SQLite

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 import os
 import sqlite3
 
-db_path = os.getenv("SQLITE_PATH", "arki_identite.db")
+db_path = os.getenv("SQLITE_PATH","arki_identite.db")
 conn = sqlite3.connect(db_path)
 cursor = conn.cursor()
 cursor.execute("CREATE TABLE IF NOT EXISTS users (user_id TEXT, data TEXT)")
 conn.commit()
-
 
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
